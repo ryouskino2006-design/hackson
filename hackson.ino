2ede9e041b2819b5f25f62b237cd23d46047eabb
ryouskino2006-design <ryouskino2006@gmail.com>
2026-05-27 00:04:18 +0900

    Update hackson.ino

--- hackson.ino
+++ hackson.ino
@@ -14,12 +14,28 @@ String melody[] = {
 0.2f,0.2f,0.2f,0.2f,0.2f,0.2f,0.2f,0.2f,0.4f,0.4f,0.4f,
 };
 
+float durationDouble[] = {
+  0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f,
+  0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f, 0.2f,
+  0.2f, 0.2f, 0.2f, 0.2f,
+  0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.2f, 0.2f, 0.2f
+};
+
+
 float startTime[]  = {
 0.0f, 0.5f, 1.0f, 1.5f, 2.0f, 2.5f, 3.0f,      
   4.0f, 4.5f, 5.0f, 5.5f, 6.0f, 6.5f, 7.0f,      
   8.0f, 9.0f, 10.0f, 11.0f,                      
   12.0f, 12.25f, 12.5f, 12.75f, 13.0f, 13.25f, 13.5f, 13.75f, 
   14.0f, 14.5f, 15.0f};
+ 
+  float startTimeDouble[] = {
+  0.0f, 0.25f, 0.5f, 0.75f, 1.0f, 1.25f, 1.5f,
+  2.0f, 2.25f, 2.5f, 2.75f, 3.0f, 3.25f, 3.5f,
+  4.0f, 4.5f, 5.0f, 5.5f,
+  6.0f, 6.125f, 6.25f, 6.375f, 6.5f, 6.625f, 6.75f, 6.875f,
+  7.0f, 7.25f, 7.5f
+};
   
   float amplitude[]  = {
   0.8f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 
@@ -56,3 +72,20 @@ if(i<28){
 }
   Serial.println();
 }
+
+void sendMelodyDouble(){
+  for(int i = 0; i < melodyLength; i++){
+    Serial.print(melody[i]);
+    Serial.print(":");
+    Serial.print(duration[i] * 0.5f);  // ★長さを半分にして送信！
+    Serial.print(":");
+    Serial.print(startTime[i] * 0.5f); // ★開始時間も半分にして送信！
+    Serial.print(":");
+    Serial.print(amplitude[i]);
+    if(i < 28) Serial.print(",");
+  }
+  Serial.println();
+}
+
+
+
